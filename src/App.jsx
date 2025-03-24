3db66aaccef3cec9f65dcabab43584ad7e048f72
Markovic Boban <sayby.19@gmail.com>
2025-03-24 16:12:46 +0100

    first frontend

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import BlogPost from "./components/BlogPost";
+import BlogPosts from "./components/BlogPosts";
+import Home from "./pages/Home";
+import MainLayout from "./layout/MainLayout";
+import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+
+          {/* DYNAMIC ROUTES */}
+          <Route path="blogposts" element={<BlogPosts />} />
+          <Route path="products/:productId" element={<BlogPost />} />
+
+          {/* NOT FOUND */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
