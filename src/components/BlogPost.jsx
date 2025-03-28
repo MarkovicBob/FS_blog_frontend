28dbe7f98395f8e733baa44675bf0b5ed13ea5f6
nidialoha <nidialoha@gmail.com>
2025-03-28 11:07:49 +0100

    Blogpost Id fixed

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,14 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast, Toaster } from "react-hot-toast";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import NotFound from "../pages/NotFound";
+import { FaRegEdit } from "react-icons/fa";
 
 function BlogPost() {
   const { id } = useParams();
   const [post, setPost] = useState(null);
-  const [editPost, setEditPost] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedPost, setEditedPost] = useState({
+    date: "",
+    title: "",
+    content: "",
+  });
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -18,17 +24,49 @@ function BlogPost() {
 
         if (res.data) {
           setPost(res.data[0]);
+          setEditedPost(res.data[0]);
         } else {
           return;
         }
       } catch (error) {
-        console.error("Error fetching post:");
+        console.error("Error fetching post:", error);
       } finally {
         setLoading(false);
       }
     };
     fetchPost();
   }, [id]);
+  // console.log(post);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleChange = (e) => {
+    setEditedPost({ ...editedPost, [e.target.name]: e.target.value });
+  };
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    console.log("Gesendete Daten:", editedPost);
+    try {
+      const res = await axios.put(
+        `http://localhost:3000/blogposts/${id}`,
+        editedPost,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      if (res.status === 200) {
+        toast.success("Post updated successfully!");
+        setPost(editedPost);
+        setIsEditing(false);
+      }
+    } catch (error) {
+      console.error("Update failed:", error);
+      toast.error("Update failed");
+    }
+  };
 
   const handleDelete = async () => {
     if (confirm("Do you really want to delete this post?")) {
@@ -38,7 +76,7 @@ function BlogPost() {
         setPost(null);
       } catch (error) {
         console.error("Error deleting post:", error);
-        toast.error("Deletion failed");
+        toast.error("Failed to delete!");
       }
     }
   };
@@ -53,19 +91,57 @@ function BlogPost() {
       {post === null ? (
         <NotFound />
       ) : (
-        <div className="justify-self-center mt-8 outline-2 rounded-2xl p-8 bg-stone-200">
-          <p>{post.date}</p>
-          <div className="flex items-center justify-between">
-            <h2 className="text-2xl font-black">{post.title}</h2>
+        <div className=" m-8">
+          <div className="flex items-center gap-8 justify-between">
+            {isEditing ? (
+              <input
+                type="date"
+                name="date"
+                value={editedPost.date.slice(0, 10)}
+                onChange={handleChange}
+              />
+            ) : (
+              <p>{post.date.slice(0, 10)}</p>
+            )}
+            <FaRegEdit onClick={handleEdit} className="cursor-pointer" />
           </div>
-          <div className="mt-6 w-[500px] flow-root">
-            <p>{post.content}</p>
-          </div>
+          <form onSubmit={handleSave}>
+            <div className=" pt-2">
+              {isEditing ? (
+                <input
+                  className="text-2xl font-black w-full"
+                  type="text"
+                  name="title"
+                  value={editedPost.title}
+                  onChange={handleChange}
+                />
+              ) : (
+                <h2 className="text-2xl font-black w-full">{post.title}</h2>
+              )}
+            </div>
+            <div className="mt-6">
+              {isEditing ? (
+                <textarea
+                  className="w-full h-auto"
+                  name="content"
+                  value={editedPost.content}
+                  onChange={handleChange}
+                ></textarea>
+              ) : (
+                <p className="w-full h-auto">{post.content}</p>
+              )}
+            </div>
 
+            {isEditing && (
+              <button
+                type="submit"
+                className="rounded-lg px-6 bg-stone-950 text-white mt-8 p-2"
+              >
+                Save Change
+              </button>
+            )}
+          </form>
           <div className="flex justify-end gap-6">
-            <button className="rounded-lg px-6 bg-stone-950 text-white mt-8 p-2">
-              Edit
-            </button>
             <button
               onClick={handleDelete}
               className="rounded-lg px-6 bg-stone-950 text-white mt-8 p-2"
